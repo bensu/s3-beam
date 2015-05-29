4494b3a20125f5895eb5bb5c1bde4dbe29859ee4
Sebastian Bensusan <sbensu@gmail.com>
2015-05-29 23:35:14 +0200

    Changed name for deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.temporary/s3-beam "0.3.0"
-  :author "Martin Klepsch <http://www.martinklepsch.org>"
-  :description "CORS Upload to S3 via Clojure(script)"
+(defproject bensu/s3-beam "0.3.0"
+  :author "Sebastian Bensusan>"
+  :description "This is a temporary deployment for Martin Klepshc's S3-Beam"
   :url "http://github.com/martinklepsch/s3-beam"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -14,4 +14,4 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :scm {:name "git"
-         :url "https://github.com/martinklepsch/s3-beam"})
+         :url "https://github.com/bensu/s3-beam"})
